a9971e3e801fb99d9754135ee80bcb4dc9790d54
AlfinAfifan <agusalfin1@gmail.com>
2023-03-14 22:07:17 +0700

    TASK 7

---
+++ JavaScript/Suit/script.js
@@ -0,0 +1,43 @@
+let tanya = true;
+while (tanya) {
+
+    // Menangkap pilihan player
+    let player = prompt('Masukkan pilihan anda: (Kertas, Batu, Gunting)');
+
+    // Menangkap pilihan komputer
+    // Membangkitkan bilangan random
+    let comp = Math.random();
+
+    if(comp < 0.34) {
+        comp = 'batu';
+    } else if (comp >= 0.34 && comp < 0.67) {
+        comp = 'kertas';
+    } else {
+        comp = 'gunting';
+    }
+
+
+    // Menentukan rules
+    let hasil = '';
+
+    if (player == comp) {
+        hasil = 'SERI'
+    } else if (player == 'kertas') {
+        hasil = (comp == 'batu') ? 'MENANG' : 'KALAH';
+    } else if (player == 'batu') {
+        hasil = (comp == 'kertas') ? 'KALAH' : 'MENANG';
+    } else if (player == 'gunting') {
+        hasil = (comp == 'kertas') ? 'MENANG' : 'KALAH';
+    } else {
+        hasil = 'Memasukan pilihan salah';
+    }
+
+
+    // Menampilkan hasil
+    alert(`Kamu memilih ${player} dan Komputer memilih ${comp}.
+Hasilnya : Kamu ${hasil}`);
+
+    tanya = confirm('Ingin bermain lagi?');
+}
+
+alert('Terimakasih sudah bermain');
